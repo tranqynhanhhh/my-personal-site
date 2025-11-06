7e148a4e5c92004947d557474d459b57f6345eae
Quynh Anh <tranquynhanh2811@gmail.com>
2025-11-06 15:06:49 +0700

    Cập nhật nội dung trang chủ

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="section">
-      <h2>🌟 Suy nghĩ của tôi</h2>
+      <h2> AELIN </h2>
       <p>Đây là nơi tôi chia sẻ những suy nghĩ, cảm xúc và góc nhìn về cuộc sống.</p>
     </div>
   );
